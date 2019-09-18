4f0395adc52284f5484364772c7dcd8cb1adbe79
ElanaFelsi <elanaehrman@gmail.com>
2019-09-18 22:21:50 +0300

    until replacing macro

--- preprocessor.py
+++ preprocessor.py
@@ -1,52 +1,75 @@
 import glob
-import re
 
 
-def add_included_file(includes, lines, name):
-    data = ""
-    for file in includes:
-        if file.startswith("<"):
-            file = re.sub('[^0-9a-zA-Z.]+', '', file)
-            the_file_to_include = open("PreprocessorTask/system" + "/" + file + ".h", "r")
-        else:
-            file = re.sub('[^0-9a-zA-Z.]+', '', file)
-            the_file_to_include = open("PreprocessorTask" + "/" + file)
-            for line in the_file_to_include:
-                data += line
-    lines = "\n".join(lines)
-    data += lines
-
-    with open(name + ".pp", 'w+') as new_src:
-        new_src.write(data)
+def get_include_path(_line):
+    if "\"" in _line:
+        return _line.split("\"", 2)[1]
+    return "system/" + _line.split("<")[1].replace(">", ".h")
 
 
-def parse_include(lines, name):
-    includes = []
+def include_list(header_filename, files):
+    with open(header_filename, 'r') as header_file:
+        for line in header_file:
+            if line.startswith("#include \""):
+                name = get_include_path(line.strip())
+                files.append(name)
+                return include_list(name, files)
+            if line.startswith("#include <"):
+                name = get_include_path(line.strip())
+                files.append(name)
+
+    return
+
+
+def get_include_content(_headers_list):
+    content = ""
+    for file in _headers_list:
+        with open(file, 'r') as f:
+            for line in f:
+                if not line.startswith("#include"):
+                    content += line
+    return content
+
+
+def get_macros(ppfile):
+    define_list = []
+    lines = ppfile.split("\n")
     for line in lines:
-        if line.startswith("#include"):
-            file_to_be_included = line.split()
-            file_to_be_included = file_to_be_included[file_to_be_included.index("#include") + 1]
-            includes.append(file_to_be_included)
-    add_included_file(includes, lines, name)
+        if line.startswith("#define"):
+            define_list.append(line)
+            ppfile.replace(line, "")
+        print(define_list)
+    return define_list
 
 
-def parse_file_name(name):
-    index_slash = name.find("\\")
-    index_p = name.find(".")
-    name = name[index_slash + 1: index_p]
-    return name
+def set_macros(define_list, ppfile):
+    for define in define_list:
+        define = define.replace("#define ", "")
+        if "__" not in define:
+            if "(" in define:
+                macro_name = define.split("(")[0]
+                parameters = ((define.split("(")[1]).split(")")[0].replace(" ", "")).split(",")
+                action = define.split(")", 1)[1]
+                for line in ppfile:
+                    if macro_name in line:
+                        line = line.replace(macro_name + "(", "")
+                        values = ((line.split(")")[0].replace(" ", "")).split(","))
+                        for i in range(len(values)):
+                            pass
+            else:
+                macro_name = define.split(" ")[0]
+                value = define.split(" ")[1]
 
 
-def open_files():
-    header_files = glob.glob('**/*.h')
-    cpp_files = glob.glob('**/*.cpp')
+for filename in glob.glob('*.cpp'):
+    with open(filename, 'r+') as cpp:
+        listofheaders = []
+        include_list(filename, listofheaders)
+        listofheaders.append(filename)
+        ppfile = ""
+        ppfile += get_include_content(listofheaders)
 
-    for file in cpp_files:
-        with open(file) as cpp_file:
-            lines = cpp_file.read().splitlines()
-            name = cpp_file.name
-            name = parse_file_name(name)
-            parse_include(lines, name)
+        define_list = get_macros(ppfile)
 
-
-open_files()
+        newppFile = open(filename.replace("cpp", "pp"), "w")
+        newppFile.write(ppfile)
