98654945fc4198a1ceaa4ee599d3c42c1877d2f2
ElanaFelsi <elanaehrman@gmail.com>
2019-09-19 15:49:57 +0300

    doesnt work prop in file yet

--- PreprocessorTask/factorial.pp
+++ PreprocessorTask/factorial.pp
@@ -1,45 +1,5 @@
 #ifndef __FUNCS__H
-#define __FUNCS__H
 
-
-enum E_STYLE
-{
-	E_START,
-	E_END
-};
-
-int		get_num();
-int		get_max(int num1, int num2);
-int		get_min(int num1, int num2);
-int	    get_sum(int num1, int num2);
-int 	factorial(int n);
-void 	print_current_time();
-void	decorate(E_STYLE style);
-
-#endif	//__FUNCS__H
-#pragma once
-
-#define KUKU "Amitai"
-#define COOKIE "Ola"
-#define FOO "Margalit"
-#define LULU "Michal"
-int factorial(int n)
-{
-	if (n > 1)
-	{
-		return(n * factorial(n - 1));
-	}
-   	else if (n == 1)
-   	{	
-    	return 1;
-   	}
-   	else
-   	{
-		return 0;
-	} 
-}
-#ifndef __FUNCS__H
-#define __FUNCS__H
 
 
 enum E_STYLE
@@ -59,10 +19,10 @@ void	decorate(E_STYLE style);
 #endif	//__FUNCS__H
 #pragma once
 
-#define "Amitai" "Amitai"
-#define "Ola" "Ola"
-#define "Margalit" "Margalit"
-#define "Michal" "Michal"
+
+
+
+
 int factorial(int n)
 {
 	if (n > 1)
--- PreprocessorTask/timer.pp
+++ PreprocessorTask/timer.pp
@@ -8,13 +8,3 @@ void print_current_time()
 
    cout << "Current time and date: " << ctime(&curtime);
 }
-
-using namespace std;
-
-void print_current_time() 
-{
-   time_t curtime;
-   time(&curtime);
-
-   cout << "Current time and date: " << ctime(&curtime);
-}
